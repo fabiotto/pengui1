825922a44d4f706c8173ece833591f067c22b825
fabiotto <fabiotavanti@Mac-mini-di-Fabio.local>
2014-12-05 00:15:07 +0100

    Automatically Committed for MakeGamesWithUs

--- Gameplay.m
+++ Gameplay.m
@@ -176,10 +176,10 @@ static const float MIN_SPEED = 5.f;
     [penguin.physicsBody applyForce:force];
     
 
-    /*// ensure followed object is in visible are when starting
+     //ensure followed object is in visible are when starting
     self.position = ccp(0, 0);
     CCActionFollow *follow = [CCActionFollow actionWithTarget:penguin worldBoundary:self.boundingBox];
-    [self runAction:follow];*/
+    [self runAction:follow];
     
     
     // ensure followed object is in visible are when starting
